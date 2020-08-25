21de2b81e11ce290a19d09eba7a1cf2739dca8ea
Nicolas Weber <nicolas.weber@neclab.eu>
2020-08-25 15:37:59 +0200

    v0.9.1-rc1

--- cmake/InjectVE.cmake
+++ cmake/InjectVE.cmake
@@ -1,1 +1,1 @@
-SET(CMAKE_MODULE_PATH /usr/local/ve/veda/cmake/nec/inject CACHE STRING "Overwrites Languages C, CXX and Fortran with native VE code" FORCE)
+SET(CMAKE_MODULE_PATH /usr/local/ve/veda/cmake/nec/inject;/usr/local/ve/veda/cmake CACHE STRING "Overwrites Languages C, CXX and Fortran with native VE code" FORCE)
