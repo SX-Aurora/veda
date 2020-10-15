03f11f7fe2cad72ee739659ee44ff681ac248c72
Erich Focht <efocht@gmail.com>
2020-10-15 13:40:04 +0200

    Setting CXX features in cmake.Right now only cxx_std_14

--- cmake/nec/inject/CMakeCXXInformation.cmake
+++ cmake/nec/inject/CMakeCXXInformation.cmake
@@ -1,2 +1,4 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../VE_CMakeInformation.cmake)
 ve_init_information("CXX" "-x c++")
+SET(CMAKE_CXX_COMPILE_FEATURES)
+LIST(APPEND CMAKE_CXX_COMPILE_FEATURES cxx_std_14)
